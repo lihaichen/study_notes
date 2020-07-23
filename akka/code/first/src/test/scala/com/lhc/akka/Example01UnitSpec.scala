8f642aa5c9d320132eab731a7fb4de67e98da638
lihaichen <li8303@163.com>
2020-07-23 22:55:57 +0800

    添加测试用例

---
+++ akka/code/first/src/test/scala/com/lhc/akka/Example01UnitSpec.scala
@@ -0,0 +1,62 @@
+package com.lhc.akka
+
+import akka.actor.typed.{ActorRef, ActorSystem, Behavior}
+import akka.actor.typed.scaladsl.Behaviors
+import org.specs2.mutable.Specification
+
+object HelloWorld {
+
+  final case class Greet(whom: String, replyTo: ActorRef[Greeted])
+
+  final case class Greeted(whom: String, from: ActorRef[Greet])
+
+  def apply(): Behavior[Greet] = Behaviors.receive { (context, message) =>
+    println(s"Hello ${message.whom}!")
+    message.replyTo ! Greeted(message.whom, context.self)
+    Behaviors.same
+  }
+}
+
+object HelloWorldBot {
+
+  def apply(max: Int): Behavior[HelloWorld.Greeted] = {
+    bot(0, max)
+  }
+
+  private def bot(greetingCounter: Int, max: Int): Behavior[HelloWorld.Greeted] =
+    Behaviors.receive { (context, message) =>
+      val n = greetingCounter + 1
+      println("self==>" + context.self)
+      println("from==>" + message.from)
+      println("Greeting {} for {}", n, message.whom)
+      if (n == max) {
+        Behaviors.stopped
+      } else {
+        message.from ! HelloWorld.Greet(message.whom, context.self)
+        bot(n, max)
+      }
+    }
+}
+
+object HelloWorldMain {
+
+  final case class SayHello(name: String)
+
+  def apply(): Behavior[SayHello] = Behaviors.setup {
+    context =>
+      val greeter = context.spawn(HelloWorld(), "greeter")
+      Behaviors.receiveMessage { message =>
+        val replyTo = context.spawn(HelloWorldBot(max = 3), message.name)
+        greeter ! HelloWorld.Greet(message.name, replyTo)
+        Behaviors.same
+      }
+  }
+}
+
+class Example01UnitSpec extends Specification {
+  val system: ActorSystem[HelloWorldMain.SayHello] =
+    ActorSystem(HelloWorldMain(), "hello")
+
+  system ! HelloWorldMain.SayHello("World")
+  system ! HelloWorldMain.SayHello("Akka")
+}
